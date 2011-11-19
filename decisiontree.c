dd6f10b2b1db748d1e013bbb69f31c82e76550ac
Hangzhong Yu <hangzhong.yu@gmail.com>
2011-11-19 17:30:42 +0800

    update the source file

--- decisiontree.c
+++ decisiontree.c
@@ -2,7 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void Init()
+{
+    //printf("hello world");
+}
+
 int main()
 {
+    TreeNode* root = NULL;
+    /*for test
+    root = (TreeNode*)malloc (sizeof(TreeNode));
+    root->infoGain = 0.0;
+    printf ("%lf\n", root->infoGain);*/
+    Init();
+
     return 0;    
 }
+
