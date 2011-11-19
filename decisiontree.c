4d46b06c77b146e9ca810411aaae32feded23274
Hangzhong Yu <hangzhong.yu@gmail.com>
2011-11-20 01:05:27 +0800

    update init

--- decisiontree.c
+++ decisiontree.c
@@ -1,25 +1,49 @@
 #include "decisiontree.h"
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-void Init()
+TreeNode* innerNodeList[MAXLEVELNUM] = { NULL };
+TreeNode* curInnerNode[MAXLEVELNUM] = { NULL };
+TreeNode* leafList = NULL;
+TreeNode* curLeaf = NULL;
+
+void Init(int argc, char* argv[])
 {
+    /*
+    int k;
+    printf("argc = %d\n", argc);
+    for (k = 0; k < argc; ++k)
+    {
+        printf("%s\n", argv[k]);        
+    }*/
     uint32_t i, j;
     uint32_t choice = 0;
-    char filename[100] = 0;
+    char filename[100] = { 0 };
 
-    for ()
+    for (i = 0; i <= MAXLEVELNUM-1; ++i)
+    {
+        innerNodeList[i] = (TreeNode*)malloc(sizeof(TreeNode));
+        memset(innerNodeList[i], 0, sizeof(TreeNode));
+
+        curInnerNode[i] = innerNodeList[i];
+    }
+
+    leafList= (TreeNode*)malloc(sizeof(TreeNode));
+    memset(leafList, 0, sizeof(TreeNode));
+    curLeaf = leafList;
     //printf("hello world");
+
 }
 
-int main()
+int main(int argc, char* argv[])
 {
     TreeNode* root = NULL;
     /*for test
     root = (TreeNode*)malloc (sizeof(TreeNode));
     root->infoGain = 0.0;
     printf ("%lf\n", root->infoGain);*/
-    Init();
+    Init(argc, argv);
 
     return 0;    
 }
